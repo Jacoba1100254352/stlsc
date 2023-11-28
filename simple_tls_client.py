2ad85d6198017ae6d7a3fcc3b65db556ed0a9b42
Jacob Anderson <jacobdanderson@gmail.com>
2023-11-27 21:27:47 -0700

    New updates, still not working

--- simple_tls_client.py
+++ simple_tls_client.py
@@ -12,18 +12,26 @@ client = None
 
 def connect(host, port):
     global client
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((host, port))
-    logging.info("Connected to server.")
+    try:
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((host, port))
+        logging.info("Connected to server.")
+    except Exception as e:
+        logging.error(f"Failed to connect because: {e}")
+        sys.exit(1)
 
 
 def send_hello():
-    hello_mes = Message(MessageType.HELLO).to_bytes()
-    client.sendall(hello_mes)
-    logging.info("Hello sent.")
-    response = Message.from_socket(client).data
-    logging.info("Nonce & Certificate received.")
-    return response
+    try:
+        hello_mes = Message(MessageType.HELLO).to_bytes()
+        client.sendall(hello_mes)
+        logging.info("Hello sent.")
+        response = Message.from_socket(client).data
+        logging.info("Nonce & Certificate received.")
+        return response
+    except Exception as e:
+        logging.error(f"Failed to send/receive hello message: {e}")
+        sys.exit(1)
 
 
 def main(host, port, filename, verbose):
@@ -38,54 +46,66 @@ def main(host, port, filename, verbose):
     server_cert = response[32:]
     server_cert_obj = utils.load_certificate(server_cert)
 
-    client_nonce = secrets.token_bytes(32)  # Using secrets for nonce generation
+    client_nonce = secrets.token_bytes(32)
     encrypted_nonce = utils.encrypt_with_public_key(client_nonce, server_cert_obj.public_key())
-    client.sendall(Message(MessageType.NONCE, encrypted_nonce).to_bytes())
-    logging.info("Encrypted nonce sent.")
+    try:
+        client.sendall(Message(MessageType.NONCE, encrypted_nonce).to_bytes())
+        logging.info("Encrypted nonce sent.")
+    except Exception as e:
+        logging.error(f"Failed to send encrypted nonce: {e}")
+        sys.exit(1)
 
     server_enc_key, server_mac_key, client_enc_key, client_mac_key = utils.generate_keys(client_nonce, server_nonce)
 
     # Receive server hash message
-    server_hash_msg = Message.from_socket(client)
+    try:
+        server_hash_msg = Message.from_socket(client)
+    except Exception as e:
+        logging.error(f"Failed to receive server hash message: {e}")
+        sys.exit(1)
 
-    # Forming cumulative data as in the working script
+    # Verification
     client_hello_message = Message(MessageType.HELLO).to_bytes()
     server_certificate_message = Message(MessageType.CERTIFICATE, response).to_bytes()
     client_nonce_message = Message(MessageType.NONCE, encrypted_nonce).to_bytes()
     cumulated_data = client_hello_message + server_certificate_message + client_nonce_message
-
     expected_server_hash = utils.mac(cumulated_data, server_mac_key)
-    logging.info(f"Server Hash (Received): {server_hash_msg.data}")
-    logging.info(f"Server Hash (Expected): {expected_server_hash}")
 
     if server_hash_msg.data != expected_server_hash:
         logging.error("Invalid server hash.")
         sys.exit(1)
 
-    client_hash = utils.mac(b''.join([Message(MessageType.HELLO).to_bytes(), response]),
-                            client_mac_key)
-    client.sendall(Message(MessageType.HASH, client_hash).to_bytes())
-    logging.info("Client hash sent.")
+    client_hash = utils.mac(b''.join([client_hello_message, response]), client_mac_key)
+    try:
+        client.sendall(Message(MessageType.HASH, client_hash).to_bytes())
+        logging.info("Client hash sent.")
+    except Exception as e:
+        logging.error(f"Failed to send client hash: {e}")
+        sys.exit(1)
 
     # Receive and process data
     received_data = b''
     sequence_number = 0
     while True:
-        data_msg = Message.from_socket(client)
-        if not data_msg or data_msg.type != MessageType.DATA:
-            break
+        try:
+            data_msg = Message.from_socket(client)
+            if not data_msg or data_msg.type != MessageType.DATA:
+                break
 
-        decrypted_data = utils.decrypt(data_msg.data, server_enc_key)
-        seq_num, data_chunk, received_mac = decrypted_data[:4], decrypted_data[4:-32], decrypted_data[-32:]
-        if int.from_bytes(seq_num, "big") != sequence_number:
-            logging.error("Invalid sequence number.")
+            decrypted_data = utils.decrypt(data_msg.data, server_enc_key)
+            seq_num, data_chunk, received_mac = decrypted_data[:4], decrypted_data[4:-32], decrypted_data[-32:]
+            if int.from_bytes(seq_num, "big") != sequence_number:
+                logging.error("Invalid sequence number.")
+                sys.exit(1)
+            calculated_mac = utils.mac(data_chunk, server_mac_key)
+            if calculated_mac != received_mac:
+                logging.error("Invalid MAC.")
+                sys.exit(1)
+            received_data += data_chunk
+            sequence_number += 1
+        except Exception as e:
+            logging.error(f"Error in data reception: {e}")
             sys.exit(1)
-        calculated_mac = utils.mac(data_chunk, server_mac_key)
-        if calculated_mac != received_mac:
-            logging.error("Invalid MAC.")
-            sys.exit(1)
-        received_data += data_chunk
-        sequence_number += 1
 
     # Save or output data
     if filename == '-':
