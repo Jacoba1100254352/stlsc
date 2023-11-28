63ea89d0e9f3eb9c6b5913598479f6666d994322
Jacob Anderson <jacobdanderson@gmail.com>
2023-11-27 21:00:32 -0700

    Reorganized, closer to working

--- simple_tls_client.py
+++ simple_tls_client.py
@@ -4,109 +4,91 @@ import sys
 import os
 import crypto_utils as utils
 from message import Message, MessageType
+import logging
 
-# Default connection parameters
-DEFAULT_PORT = 8087
-DEFAULT_HOST = 'localhost'
-TIMEOUT_SECONDS = 10  # Timeout for socket operations
+# Global client socket
+client = None
 
-# Parse command line arguments
+def connect(host, port):
+    global client
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((host, port))
+    logging.info("Connected to server.")
+
+def send_hello():
+    hello_mes = Message(MessageType.HELLO).to_bytes()
+    client.sendall(hello_mes)
+    logging.info("Hello sent.")
+    response = Message.from_socket(client).data
+    logging.info("Nonce & Certificate received.")
+    return response
+
+def main(host, port, filename, verbose):
+    if verbose:
+        logging.basicConfig(level=logging.INFO)
+
+    connect(host, port)
+    response = send_hello()
+
+    server_nonce = response[:32]
+    server_cert = response[32:]
+    server_cert_obj = utils.load_certificate(server_cert)
+
+    client_nonce = os.urandom(32)
+    encrypted_nonce = utils.encrypt_with_public_key(client_nonce, server_cert_obj.public_key())
+    client.sendall(Message(MessageType.NONCE, encrypted_nonce).to_bytes())
+    logging.info("Encrypted nonce sent.")
+
+    server_enc_key, server_mac_key, client_enc_key, client_mac_key = utils.generate_keys(client_nonce, server_nonce)
+
+    server_hash_msg = Message.from_socket(client)
+    expected_server_hash = utils.mac(b''.join([Message(MessageType.HELLO).to_bytes(), response]),
+                                     server_mac_key)
+    if server_hash_msg.data != expected_server_hash:
+        logging.error("Invalid server hash.")
+        sys.exit(1)
+
+    client_hash = utils.mac(b''.join([Message(MessageType.HELLO).to_bytes(), response]),
+                            client_mac_key)
+    client.sendall(Message(MessageType.HASH, client_hash).to_bytes())
+    logging.info("Client hash sent.")
+
+    # Receive and process data
+    received_data = b''
+    sequence_number = 0
+    while True:
+        data_msg = Message.from_socket(client)
+        if not data_msg or data_msg.type != MessageType.DATA:
+            break
+
+        decrypted_data = utils.decrypt(data_msg.data, server_enc_key)
+        seq_num, data_chunk, received_mac = decrypted_data[:4], decrypted_data[4:-32], decrypted_data[-32:]
+        if int.from_bytes(seq_num, "big") != sequence_number:
+            logging.error("Invalid sequence number.")
+            sys.exit(1)
+        calculated_mac = utils.mac(data_chunk, server_mac_key)
+        if calculated_mac != received_mac:
+            logging.error("Invalid MAC.")
+            sys.exit(1)
+        received_data += data_chunk
+        sequence_number += 1
+
+    # Save or output data
+    if filename == '-':
+        sys.stdout.buffer.write(received_data)
+    else:
+        with open(filename, 'wb') as file:
+            file.write(received_data)
+    logging.info("Data received and saved")
+
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Simple TLS Client')
     parser.add_argument('file', help='The file name to save to. Use - for stdout.')
-    parser.add_argument('-p', '--port', type=int, default=DEFAULT_PORT, help='Port to connect to.')
-    parser.add_argument('--host', default=DEFAULT_HOST, help='Hostname to connect to.')
+    parser.add_argument('-p', '--port', type=int, default=8087, help='Port to connect to.')
+    parser.add_argument('--host', default='localhost', help='Hostname to connect to.')
     parser.add_argument('-v', '--verbose', action='store_true', help='Turn on debugging output.')
     return parser.parse_args()
 
-# Print debug message if verbose mode is on
-def debug_print(verbose, message):
-    if verbose:
-        print(message)
-
-# Main program
-def main():
-    args = parse_arguments()
-
-    # Establish connection with timeout
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.settimeout(TIMEOUT_SECONDS)
-        debug_print(args.verbose, f"Connecting to {args.host}:{args.port}")
-        s.connect((args.host, args.port))
-
-        # Send Hello message
-        debug_print(args.verbose, "Sending Hello message")
-        s.send(Message(MessageType.HELLO).to_bytes())
-
-        # Receive and verify certificate
-        debug_print(args.verbose, "Waiting for Certificate message")
-        server_cert_msg = Message.from_socket(s)
-        if server_cert_msg is None:
-            raise Exception("No data received from server, or connection closed unexpectedly")
-        if server_cert_msg.type != MessageType.CERTIFICATE:
-            raise Exception("Expected a certificate message, received something else")
-        server_nonce, server_cert = server_cert_msg.data[:32], server_cert_msg.data[32:]
-        server_cert_obj = utils.load_certificate(server_cert)
-        if not server_cert_obj:
-            raise Exception("Invalid server certificate")
-
-        # Send encrypted nonce
-        client_nonce = os.urandom(32)
-        encrypted_nonce = utils.encrypt_with_public_key(client_nonce, server_cert_obj.public_key())
-        debug_print(args.verbose, "Sending encrypted nonce")
-        s.send(Message(MessageType.NONCE, encrypted_nonce).to_bytes())
-
-        # Generate keys
-        server_enc_key, server_mac_key, client_enc_key, client_mac_key = utils.generate_keys(client_nonce, server_nonce)
-
-        # Receive server hash and validate
-        debug_print(args.verbose, "Waiting for Server Hash message")
-        server_hash_msg = Message.from_socket(s)
-        if server_hash_msg is None or server_hash_msg.type != MessageType.HASH:
-            raise Exception("Expected a hash message, received something else or no message")
-        expected_server_hash = utils.mac(b''.join([Message(MessageType.HELLO).to_bytes(),
-                                                   server_cert_msg.to_bytes(),
-                                                   Message(MessageType.NONCE, encrypted_nonce).to_bytes()]),
-                                         server_mac_key)
-        if server_hash_msg.data != expected_server_hash:
-            raise Exception("Invalid server hash")
-
-        # Send client hash
-        client_hash = utils.mac(b''.join([Message(MessageType.HELLO).to_bytes(),
-                                          server_cert_msg.to_bytes(),
-                                          Message(MessageType.NONCE, encrypted_nonce).to_bytes()]),
-                                client_mac_key)
-        debug_print(args.verbose, "Sending Client Hash message")
-        s.send(Message(MessageType.HASH, client_hash).to_bytes())
-
-        # Receive and process data
-        debug_print(args.verbose, "Receiving data messages")
-        received_data = b''
-        sequence_number = 0
-        while True:
-            data_msg = Message.from_socket(s)
-            if data_msg is None:
-                raise Exception("No data received from server, or connection closed unexpectedly")
-            if data_msg.type != MessageType.DATA:
-                break  # Assuming the non-data message signifies the end of data transmission
-
-            decrypted_data = utils.decrypt(data_msg.data, server_enc_key)
-            seq_num, data_chunk, received_mac = decrypted_data[:4], decrypted_data[4:-32], decrypted_data[-32:]
-            if int.from_bytes(seq_num, "big") != sequence_number:
-                raise Exception("Invalid sequence number")
-            calculated_mac = utils.mac(data_chunk, server_mac_key)
-            if calculated_mac != received_mac:
-                raise Exception("Invalid MAC")
-            received_data += data_chunk
-            sequence_number += 1
-
-        # Save or output data
-        if args.file == '-':
-            sys.stdout.buffer.write(received_data)
-        else:
-            with open(args.file, 'wb') as file:
-                file.write(received_data)
-        debug_print(args.verbose, "Data received and saved")
-
 if __name__ == "__main__":
-    main()
+    args = parse_arguments()
+    main(args.host, args.port, args.file, args.verbose)
